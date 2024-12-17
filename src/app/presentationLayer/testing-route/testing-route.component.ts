98dc75fad24428747e7c0dd176046f86ed6875db
BesneiDavid <besneidavid92@yahoo.com>
2024-12-17 19:04:05 +0200

    feature/17122024-fisierul-de-routing

---
+++ src/app/presentationLayer/testing-route/testing-route.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-route',
+  standalone: true,
+  imports: [],
+  templateUrl: './testing-route.component.html',
+  styleUrl: './testing-route.component.scss'
+})
+export class TestingRouteComponent {
+
+}
